74611306d6c4527d7476fe4eabda0656f29833a2
Haafii <hafismuhammedtk@gmail.com>
2023-06-19 23:51:26 +0530

    ui changed

--- src/pages/EnterUrl.jsx
+++ src/pages/EnterUrl.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 function EnterUrl() {
   const [url, setUrl] = useState('');
@@ -11,18 +11,21 @@ function EnterUrl() {
   };
 
   const handleButtonClick = () => {
-    setIsClicked(!isClicked);
+    setIsClicked(true);
     console.log('URL:', url);
   };
 
   const containerVariants = {
     hidden: {
       opacity: 0,
+      scale: 0.8,
     },
     visible: {
       opacity: 1,
+      scale: 1,
       transition: {
         duration: 0.5,
+        delay: 0.2,
       },
     },
   };
@@ -30,9 +33,11 @@ function EnterUrl() {
   const resultVariants = {
     hidden: {
       opacity: 0,
+      x: -20,
     },
     visible: {
       opacity: 1,
+      x: 0,
       transition: {
         duration: 0.5,
       },
@@ -60,39 +65,49 @@ function EnterUrl() {
         animate="visible"
         variants={containerVariants}
       >
-        <h1 className="text-2xl font-semibold mb-6">URL Checker</h1>
-        <input
+        <motion.h1
+          className="text-2xl font-semibold mb-6"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0, transition: { delay: 0.3 } }}
+        >
+          URL Checker
+        </motion.h1>
+        <motion.input
           type="text"
           placeholder="Enter URL"
           className="w-1/2 px-4 py-2 rounded-lg border-2 border-purple-400 mb-4 focus:outline-none focus:ring-2 focus:ring-purple-400"
           value={url}
           onChange={handleInputChange}
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0, transition: { delay: 0.4 } }}
         />
         <motion.button
           className="w-1/6 bg-purple-500 hover:bg-purple-600 text-white font-semibold py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-400"
           onClick={handleButtonClick}
-          initial="hidden"
-          animate="visible"
-          variants={resultVariants}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { delay: 0.5 } }}
         >
-          {isClicked ? 'Reset' : 'Check'}
+          Check
         </motion.button>
-        <AnimatePresence>
-          {isClicked && (
+        {isClicked && (
+          <motion.div
+            className="w-3/4 flex flex-grow h-6 mt-4 flex-col gap-2 bg-gray-300"
+            initial="hidden"
+            animate="visible"
+            variants={resultVariants}
+          >
             <motion.div
-              className="w-3/4 flex flex-grow h-6 mt-4 flex-col gap-2 md:bg-gray-300 md:shadow-2xl"
-              initial="hidden"
-              animate="visible"
-              exit="hidden"
-              variants={resultVariants}
+              className="mt-8 ml-4"
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0, transition: { delay: 0.3 } }}
             >
-              <div className="mt-8 ml-4">
-                <h1 className="text-gray-700 font-bold">Url : {url}</h1>
-                <p>Details</p>
-              </div>
+              <motion.h1 className="text-gray-700 font-bold">{`Url: ${url}`}</motion.h1>
+              <motion.p initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 0.4 } }}>
+                Details
+              </motion.p>
             </motion.div>
-          )}
-        </AnimatePresence>
+          </motion.div>
+        )}
       </motion.div>
     </motion.div>
   );
