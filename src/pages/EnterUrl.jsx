3e464d76edd4eaac9e49d26ad55257e39260f1e6
Haafii <hafismuhammedtk@gmail.com>
2023-06-19 23:23:44 +0530

    ui design finished

--- src/pages/EnterUrl.jsx
+++ src/pages/EnterUrl.jsx
@@ -1,62 +1,47 @@
-// import React, { useState } from 'react'
-
-// function EnterUrl() {
-//   const [url, setUrl] = useState('');
-
-//   const handleInputChange = (event) => {
-//     setUrl(event.target.value);
-//   };
-
-//   const handleButtonClick = () => {
-//     console.log('URL:', url);
-//   };
-//   return (
-//     <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
-//       <div className="max-w-md p-6 bg-white rounded-lg shadow-lg">
-//         <h1 className="text-2xl font-semibold mb-6">URL Checker</h1>
-//         <input
-//           type="text"
-//           placeholder="Enter URL"
-//           className="w-full px-4 py-2 rounded-lg border-2 border-purple-400 mb-4 focus:outline-none focus:ring-2 focus:ring-purple-400"
-//           value={url}
-//           onChange={handleInputChange}
-//         />
-//         <button
-//           className="w-full bg-purple-500 hover:bg-purple-600 text-white font-semibold py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-400"
-//           onClick={handleButtonClick}
-//         >
-//           Check
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default EnterUrl
-
-
-
-
-
-
-
 import React, { useState } from 'react';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 
 function EnterUrl() {
   const [url, setUrl] = useState('');
+  const [isClicked, setIsClicked] = useState(false);
 
   const handleInputChange = (event) => {
     setUrl(event.target.value);
+    setIsClicked(false);
   };
 
   const handleButtonClick = () => {
+    setIsClicked(!isClicked);
     console.log('URL:', url);
+  };
+
+  const containerVariants = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+
+  const resultVariants = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+      },
+    },
   };
 
   return (
     <motion.div
-      className="flex items-center justify-center min-h-screen"
+      className={`flex ${isClicked ? 'items-top' : 'items-center'} justify-center min-h-screen`}
       style={{
         background: 'linear-gradient(to right, #ff4b1f, #1fddff)',
       }}
@@ -69,22 +54,46 @@ function EnterUrl() {
         repeatType: 'reverse',
       }}
     >
-      <div className="max-w-md p-6 items-center flex flex-col">
-        <h1 className="text-2xl font-semibold mb-6 " >URL Checker</h1>
+      <motion.div
+        className="w-full p-6 items-center flex flex-col"
+        initial="hidden"
+        animate="visible"
+        variants={containerVariants}
+      >
+        <h1 className="text-2xl font-semibold mb-6">URL Checker</h1>
         <input
           type="text"
           placeholder="Enter URL"
-          className="w-full px-4 py-2 rounded-lg border-2 border-purple-400 mb-4 focus:outline-none focus:ring-2 focus:ring-purple-400"
+          className="w-1/2 px-4 py-2 rounded-lg border-2 border-purple-400 mb-4 focus:outline-none focus:ring-2 focus:ring-purple-400"
           value={url}
           onChange={handleInputChange}
         />
-        <button
-          className="w-full bg-purple-500 hover:bg-purple-600 text-white font-semibold py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-400"
+        <motion.button
+          className="w-1/6 bg-purple-500 hover:bg-purple-600 text-white font-semibold py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-400"
           onClick={handleButtonClick}
+          initial="hidden"
+          animate="visible"
+          variants={resultVariants}
         >
-          Check
-        </button>
-      </div>
+          {isClicked ? 'Reset' : 'Check'}
+        </motion.button>
+        <AnimatePresence>
+          {isClicked && (
+            <motion.div
+              className="w-3/4 flex flex-grow h-6 mt-4 flex-col gap-2 bg-gray-300 shadow-2xl"
+              initial="hidden"
+              animate="visible"
+              exit="hidden"
+              variants={resultVariants}
+            >
+              <div className="mt-8 ml-4">
+                <h1 className="text-gray-700 font-bold">Url : {url}</h1>
+                <p>Details</p>
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </motion.div>
     </motion.div>
   );
 }
